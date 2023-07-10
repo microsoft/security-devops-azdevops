f0502a17140633bbeed611f06447a15b6cd83498
Laveesh Rohra <larohra@microsoft.com>
2023-07-10 08:01:39 -0700

    Improve logging and write tests (#64)

--- test/MicrosoftSecurityDevOps/v1/msdo-helpers.tests.ts
+++ test/MicrosoftSecurityDevOps/v1/msdo-helpers.tests.ts
@@ -1,7 +1,9 @@
 import { stagingDirectory } from '../../testCommon';
 import * as path from 'path';
 import * as assert from 'assert';
+import { Writable } from 'node:stream';
 const helpers = require(path.join(stagingDirectory, 'MicrosoftSecurityDevOps', 'v1', 'msdo-helpers'));
+import os from 'os';
 
 describe('Common module tests', function () {
 
@@ -26,5 +28,31 @@ describe('Common module tests', function () {
         const expected = "MS43LjIKVmVyc2lvbjogMC4wLjEKOjo6RXZlbnRzOgpldmVudHM6MTIzCjo6OkltYWdlczoKaW1hZ2VzOjU2Nw==";
         const result = helpers.getEncodedContent(dockerVersion, events, images, taskVersion);
         assert.equal(result, expected, "encoded content didn't match");
+    });
+
+    it('should write data to the output stream', () => {
+        const writable = new Writable({
+            write(chunk, encoding, callback) {
+                assert.equal(chunk.toString(), 'test' + os.EOL);
+                callback();
+            }
+        });
+
+        helpers.writeToOutStream('test', writable);
+    });
+
+    it('should write data to the standard output stream if no output stream is specified', () => {
+        const writable = new Writable({
+            write(chunk, encoding, callback) {
+                assert.equal(chunk.toString(), 'test' + os.EOL);
+                callback();
+            }
+        });
+
+        const stdoutWrite = process.stdout.write;
+        process.stdout.write = writable.write.bind(writable);
+
+        helpers.writeToOutStream('test');
+        process.stdout.write = stdoutWrite;
     });
 });
