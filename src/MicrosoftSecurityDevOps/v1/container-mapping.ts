21fa666eeb5205a7579c7f8f45ebe5ce8c800345
Anais Miller <Anais.Miller@microsoft.com>
2024-12-11 20:27:21 +0000

    Merged PR 18844: Add ADO CM Auth Push
    Add Auth Push for ADO Container Mapping
    
    ----
    
    Tested in my own org with the test extension, verified the logs in app insights as well
    
    -----
    version with lots of print statements:
    ![image.png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image.png)
    
    -----
    Current version:
    ![image (9).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%289%29.png)
    
    App insights afd:
    ![image (2).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%282%29.png)
    
    App insights global router
    ![image (3).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%283%29.png)
    
    -----
    When there is no docker image:
    ![image (4).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%284%29.png)
    ![image (5).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%285%29.png)
    
    ----
    Graph-builder correctly processing messages and edges are being created and sent to Cloud Map:
    ![image (7).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%287%29.png)
     ![image (6).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%286%29.png)
    
    ----
    New task
    ![image (11).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%2811%29.png)
    with url
    ![image (10).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%2810%29.png)
    no url
    ![image (12).png](https://dev.azure.com/SecurityTools/9b58857a-5590-43d9-a06f-4b6bc290db3e/_apis/git/repositories/0787960b-e6b5-4010-b2a1-c7a83bf96d19/pullRequests/18844/attachments/image%20%2812%29.png)
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature: Added ADO CM Auth Push functionality.
    
    #### PR Summary
    This pull request introduces the ADO CM Auth Push feature, which includes collecting Docker data and sending it to an API endpoint with authentication.
    - `src/MicrosoftSecurityDevOps/v1/container-mapping.ts`: Added meth...

--- src/MicrosoftSecurityDevOps/v1/container-mapping.ts
+++ src/MicrosoftSecurityDevOps/v1/container-mapping.ts
@@ -3,6 +3,11 @@ import { IMicrosoftSecurityDevOps } from "./msdo-interface";
 import tl = require('azure-pipelines-task-lib/task');
 import { CommandExecutor, ICommandResult } from "./command-executor";
 import {v4 as uuidv4} from 'uuid';
+import * as os from 'os';
+import * as https from "https";
+
+const ContainerMappingUrlProd: string = "https://dfdinfra-afdendpoint-prod-d5fqbucbg7fue0cf.z01.azurefd.net/azuredevops/v1/container-mappings";
+const TokenApiVersion: string = "api-version=7.1-preview.1";
 
 /**
  * Represents the tasks for container mapping that are used to fetch Docker images pushed in a job run.
@@ -26,7 +31,7 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
     }
 
     /*
-    * Using the start time, fetch the docker events and docker images in this job run and log the encoded output
+    * Using the start time, fetch the docker events and docker images in this job run and log the encoded output.
     */
     private async runPostJob() {
         let startTime = tl.getVariable(Constants.PreJobStartTime);
@@ -34,6 +39,12 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
             startTime = new Date(new Date().getTime() - 10000).toISOString();
             writeToOutStream(Constants.PreJobStartTime + " variable not set/undefined, using now-10secs ");
         }
+
+        let reportData = {
+            dockerVersion: "",
+            dockerEvents: [],
+            dockerImages: []
+        };
 
         // Initialize the commands 
         let dockerVersionCmd = new CommandExecutor('docker', '--version');
@@ -45,6 +56,9 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
         let evPromise : Promise<ICommandResult> = eventsCmd.execute();
         let imPromise : Promise<ICommandResult> = imagesCmd.execute();
 
+        // Get the OIDC token
+        let bearerTokenPromise: Promise<string> = this.GetOIDCToken();            
+
         // Wait for Docker version
         let dockerVersion: ICommandResult = await dvPromise;
         if (dockerVersion.code != 0) {
@@ -53,6 +67,7 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
         }
         const cleanedDockerVersion = CommandExecutor.removeCommandFromOutput(dockerVersion.output);
         tl.debug(`Docker Version: ${cleanedDockerVersion}`);
+        reportData.dockerVersion = cleanedDockerVersion;
 
         // Wait for Docker events command to verify any images were built on this run
         let events: ICommandResult = await evPromise;
@@ -64,12 +79,13 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
         var images: ICommandResult;
         if (!cleanedEventsOutput) {
             tl.debug(`No Docker events found`);
-            // Log a detail if no events found. We will check for this DetailTimeline record from our backend to reduce calls to ADO REST API to be mindful of Rate Limits.
-            tl.logDetail(uuidv4(), "No Docker events found", null, "NoDockerEvents", "NoDockerEvents", 999);
+            // Log a detail if no events found. We will check for this DetailTimeline record from our backend to reduce calls to ADO REST API to be mindful of Rate Limits., remove after oidc
+            tl.logDetail(uuidv4(), "No Docker events found", null, "NoDockerEvents", "NoDockerEvents", 999); //remove after oidc
             // Initialize an empty Command Result for Docker images
             images = <ICommandResult>{ code: 0, output: "" };
         }
         else {
+            reportData.dockerEvents = cleanedEventsOutput.split(os.EOL);
             // Wait for Docker images command only if events were found
             images = await imPromise;
             if (images.code != 0) {
@@ -77,20 +93,134 @@ export class ContainerMapping implements IMicrosoftSecurityDevOps {
             }
         }
 
+        const cleanedImagesOutput = CommandExecutor.removeCommandFromOutput(images.output);
+        reportData.dockerImages = cleanedImagesOutput.split(os.EOL);
+
+        //remove after oidc
         writeToOutStream(getEncodedContent(
             cleanedDockerVersion, 
             cleanedEventsOutput, 
-            CommandExecutor.removeCommandFromOutput(images.output)));
+            cleanedImagesOutput));
+        //remove after oidc
+
+        tl.debug(JSON.stringify(reportData));
+
+        // Upload the data
+        tl.debug(`Finished data collection, starting API calls`);        
+
+        let bearerToken: string = await bearerTokenPromise
+            .then((token) => {  
+                if (!token) {
+                    throw new Error("Empty OIDC token received");
+                }
+                return token; 
+            })
+            .catch((error) => {
+                throw new Error("Unable to get token: " + error);
+            });        
+
+        const sendStartTime = new Date().toISOString();
+        await this.SendReport(JSON.stringify(reportData), bearerToken);
+        const sendEndTime = new Date().toISOString();
+        //writeToOutStream("Container Mapping data sent successfully in " + (new Date(sendEndTime).getTime() - new Date(sendStartTime).getTime()) + "ms"); //readd after oidc
+        writeToOutStream(`##[debug]Container Mapping data sent successfully in ${(new Date(sendEndTime).getTime() - new Date(sendStartTime).getTime())}ms`); //remove after oidc
     }
 
     /*
-    * Run the specified function based on the task type
+    * Get the OIDC Token. Returns the token as a string.
+    */
+    private async GetOIDCToken(): Promise<string>
+    {
+        // https://learn.microsoft.com/rest/api/azure/devops/distributedtask/oidctoken/create?view=azure-devops-rest-7.1
+        let collectionUri = tl.getVariable('SYSTEM_CollectionUri');        
+        let teamProjectId = tl.getVariable('SYSTEM_TeamProjectId');
+        let hostType = tl.getVariable('SYSTEM_HostType');
+        let planId = tl.getVariable('SYSTEM_PlanId');
+        let jobId = tl.getVariable('SYSTEM_JobId');        
+        let uri = collectionUri + teamProjectId + "/_apis/distributedtask/hubs/" + hostType + "/plans/" + planId + "/jobs/" + jobId + "/oidctoken?" + TokenApiVersion;
+
+        let bearerToken = tl.getVariable('SYSTEM_ACCESSTOKEN');
+        let data = JSON.stringify({authorizationId: "00000000-0000-0000-0000-000000000000"});
+        
+        return this.PostData(uri, data, bearerToken, true)
+            .then((response) => JSON.parse(response))
+            .then((json) => json.oidcToken)
+            .catch((reason) => { throw new Error("Unable to get token: " + reason); });
+    }
+
+    /*
+    * Upload the data to Defender for DevOps. Returns the status code of the API call.
+    */
+    private async SendReport(reportData: string, bearerToken: string): Promise<number>
+    {
+        let alternateDevOpsServer: string = tl.getInput('alternateDevOpsServer');
+        let containerMappingUrl: string = (alternateDevOpsServer && alternateDevOpsServer.length > 0) ? alternateDevOpsServer : ContainerMappingUrlProd;
+
+        return this.PostData(containerMappingUrl, reportData, bearerToken, false)
+            .then(response => response.statusCode)
+            .catch((reason) => { throw new Error("Unable to post data: " + reason); })
+    }
+
+    /*
+    * Post Request to the specified URI with the data and auth token provided. Returns the response object.
+    */
+    private async PostData(uri: string, data: string, auth: string, returnData: boolean): Promise<any>
+    {
+        return new Promise(async (resolve, reject) => {
+            let options = {
+                method: 'POST',
+                timeout: 2500,
+                body: data,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + auth,
+                    'Content-Length': '' + data.length
+                }
+            };
+            writeToOutStream(`##[debug]${options['method'].toUpperCase()} ${uri}`);
+
+            const req = https.request(uri, options, (res) => {
+                let resData = '';
+                res.on('data', (chunk) => {
+                    resData += chunk.toString();
+                });
+                
+                res.on('end', () => {
+                    if (res.statusCode < 200 || res.statusCode >= 300) {
+                        return reject(`Received Failed Status code when calling url: ${res.statusCode} ${resData}`);
+                    }
+                    writeToOutStream(`##[debug]Received Status code: ${res.statusCode} and Status message: ${res.statusMessage}`);
+
+                    // Return the data if requested
+                    if (returnData) {
+                        resolve(resData);
+                    }
+                    // Return client response otherwise
+                    resolve(res);
+                });
+                
+                res.on('error', (error) => {
+                    reject(new Error(`Error calling url error: ${error}`));
+                });
+            });
+
+            req.on('error', (error) => {
+                reject(new Error(`Error calling url: ${error}`));
+            });
+
+            req.write(data);
+            req.end();
+        });
+    }
+
+    /*
+    * Run the specified function based on the task type.
     */
     async run() {
         // Group command adds a collapsible section in the logs - https://learn.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands?view=azure-devops&tabs=bash#formatting-commands
         writeToOutStream("##[group]This task was injected as part of Microsoft Defender for DevOps enablement- https://go.microsoft.com/fwlink/?linkid=2231419");
-        // This section is used as a delimiter while fetching logs from the REST API in our backend, do not modify
-        writeToOutStream("##[section]:::::");
+        // This section is used as a delimiter while fetching logs from the REST API in our backend, remove after oidc
+        writeToOutStream("##[section]:::::"); //remove after oidc
 
         try {
             switch (this.commandType) {
